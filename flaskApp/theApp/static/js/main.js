8314e4838bc5fbb066504f98f695a0f5305bec19
jonroberts <jproberts00@gmail.com>
2013-06-27 14:11:57 -0700

    fixed null entry error from titles in main.js

--- flaskApp/theApp/static/js/main.js
+++ flaskApp/theApp/static/js/main.js
@@ -19,15 +19,17 @@
         var song_list = _.template("<li><a href='http://www.youtube.com/results?search_query=<%= song %>'><%= song %></a></li>");
 
         _.each(JSON.parse(data), function(result) {
+			if(result.titles.length>0){
 
-          $target.find('ul.nav-tabs').append(template(result));
-          $target.find('div.tab-content').append(content({phrase_id:result.phrase_id,story_link:result.top_link.aggregate_url,title:result.titles[0]}));
-
-          _.each(result.songs, function(s) {
-            $target.find('div.tab-content #' + result.phrase_id + ' ul').append(
-              song_list({ song: s.title })
-            );
-          });
+	          $target.find('ul.nav-tabs').append(template(result));
+	          $target.find('div.tab-content').append(content({phrase_id:result.phrase_id, story_link:result.top_link.aggregate_url, title:result.titles[0]}));
+	
+	          _.each(result.songs, function(s) {
+	            $target.find('div.tab-content #' + result.phrase_id + ' ul').append(
+	              song_list({ song: s.title })
+	            );
+	          });
+          }
         });
         /*var template = _.template("<li><a href='#<%= phrase_id %>' data-toggle='tab'><%= phrase %></a></li>");
         var content = _.template("<div class='tab-pane' id='<%= phrase_id %>'><ul class='titles'></ul></div>");
